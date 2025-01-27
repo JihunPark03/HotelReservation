fa27cd92d32eeec7c41d6115d1e0c80402c4dcbb
Jihun Park <65612489+JihunPark03@users.noreply.github.com>
2025-01-27 14:22:35 +0900

    Delete HotelReservation code directory

--- HotelReservation code/dev_program_DB/mydb/runManager.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Copyright(C) 2007 National Institute of Informatics, All rights reserved.
-
-# Run the HSQLDB Database Manager
-java -classpath ../lib/hsqldb.jar org.hsqldb.util.DatabaseManager -url jdbc:hsqldb:hsql://localhost
--- HotelReservation code/dev_program_DB/mydb/runServer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Copyright(C) 2007 National Institute of Informatics, All rights reserved.
-
-# Start the HSQLDB server with the specified database
-java -classpath ../lib/hsqldb.jar org.hsqldb.Server -database mydb
